79d02c0954280d97d65c70e0b62fe65497781d33
JaySheth51 <jaysheth51@gmail.com>
2021-07-19 18:02:36 +0530

    fetching related products

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,19 +33,21 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
 
-  Product.findByPk(prodId)
-    .then(product => {
-      if(!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product,
-      });
-    })
-    .catch(err => console.log(err));
+  req.user
+    .getProducts({ where: { id: prodId }})
+      .then(products => {
+        const product = products[0];
+        if(!product) {
+          return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+          pageTitle: 'Edit Product',
+          path: '/admin/edit-product',
+          editing: editMode,
+          product: product,
+        });
+      })
+      .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -70,7 +72,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render('admin/products', {
         prods: products,
